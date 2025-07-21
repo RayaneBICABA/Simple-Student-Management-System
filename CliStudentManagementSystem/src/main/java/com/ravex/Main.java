1669664ed1099b1ab2c989aca01dfe6267b3d3ad
RayaneBICABA <rayanebicaba.dev@gmail.com>
2025-07-21 23:29:58 +0000

    student management system CLI Init

---
+++ CliStudentManagementSystem/src/main/java/com/ravex/Main.java
@@ -0,0 +1,18 @@
+package com.ravex;
+
+import java.util.Scanner;
+
+public class Main{
+    public static void main(String[]args){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("====== Welcome to Ravex Student Management System ==========");
+        System.out.println(" 1- Add Student \n 2- Display all students \n 3- Compute general average 4- Display the best student \n 5- Search student \n 6- Filter students \n 7- Exit");
+        System.out.println("Choose:__");
+        int choice = sc.nextInt();
+
+        // Add Student function
+        
+
+        // Display all student function
+    }
+}
